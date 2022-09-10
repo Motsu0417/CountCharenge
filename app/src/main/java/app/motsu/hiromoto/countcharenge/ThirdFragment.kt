819a6d4f4619722d1e585b9a2ce51122163184be
Kaito Dogi <kendobu0405@gmail.com>
2022-09-10 12:53:06 +0900

    review: ThirdFragment

--- app/src/main/java/app/motsu/hiromoto/countcharenge/ThirdFragment.kt
+++ app/src/main/java/app/motsu/hiromoto/countcharenge/ThirdFragment.kt
@@ -9,6 +9,9 @@ import app.motsu.hiromoto.countcharenge.databinding.FragmentThirdBinding
 
 class ThirdFragment : Fragment() {
 
+    // Doggy: 実は Fragment では、メモリリークを防ぐために、View Binding を使用するときに Activity とは異なる書き方をします…！
+    //  公式ドキュメントを参照しつつ、書き換えてみよう！
+    //  https://developer.android.com/topic/libraries/view-binding?hl=ja#fragments
     lateinit var binding:FragmentThirdBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +28,8 @@ class ThirdFragment : Fragment() {
         binding = FragmentThirdBinding.inflate(layoutInflater)
         return binding.root
     }
+
+    // Doggy: 本来はここで、 カウント画面に飛ぶ感じかな？？
+    //  Fragment における View の操作は onViewCreated で実装します！ぜひ実装してみよう！！
 
 }
